ce1f5198f362a2d87652e13706347a5c0a3b67a2
Ceiber Garibay <ceiber@hotmail.com>
2021-09-15 12:47:33 -0500

    Inital commit for CleanArch.Mvc projects

---
+++ CleanArch/CleanArch.Mvc/Data/Migrations/20210915171236_InitalMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArch.Mvc.Data.Migrations
+{
+    public partial class InitalMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
